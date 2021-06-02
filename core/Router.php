6c3b9d2b5340621ec1bb3d844b472054e401ca0d
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-06-02 18:58:17 +0000

    Update URL matching logic in Router class
    Get the controller, action, etc., from a URL with a variable structure

--- core/Router.php
+++ core/Router.php
@@ -19,12 +19,20 @@ class Router
     /**
      * Add a route to the routing table.
      *
+     * Before being added to the routing table,
+     * the route string is converted to a regular expression,
+     * and can contain variables in the {variable} format.
+     *
      * @param string $route The route URL.
      * @param array $params Parameters (controller, action, etc.).
      * @return void
      */
-    public function add(string $route, array $params)
+    public function add(string $route, array $params = [])
     {
+        $route = str_replace('/', '\/', $route);
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+        $route = "/^$route$/i";
+
         $this->routes[$route] = $params;
     }
 
@@ -39,17 +47,26 @@ class Router
     }
 
     /**
-     * Match a URL to the routes in the routing table,
-     * setting the parameters property if a route is found.
+     * Match a URL to the routes in the routing table.
+     *
+     * Setting the parameters property if a route is found.
      *
      * @param string $url The route URL.
      * @return bool True if match found, false otherwise.
      */
     public function match(string $url): bool
     {
-        if (array_key_exists($url, $this->routes)) {
-            $this->params = $this->routes[$url];
-            return true;
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+                $this->params = $params;
+                return true;
+            }
         }
         return false;
     }
