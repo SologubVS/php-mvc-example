f8024891b7e881baa786b2c739a754b1043243bd
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-06-03 10:31:27 +0000

    Add two initial controllers

---
+++ app/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+class Home
+{
+    /**
+     * Show the index page.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        echo "Hello from the index action in the Home controller!";
+    }
+}
