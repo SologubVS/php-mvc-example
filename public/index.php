7de42de74142243c3401a1b4fb8706a9bd78d158
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-06-03 12:34:57 +0000

    Add autoloader to front controller

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "Hello, World!";
+spl_autoload_register(function ($class) {
+    $roots = [
+        __DIR__ . '/../app/'  => 'App\\',
+        __DIR__ . '/../core/' => 'Core\\',
+    ];
+    foreach ($roots as $baseDir => $prefix) {
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            continue;
+        }
+        $relativeClass = substr($class, $len);
+
+        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+    }
+});
