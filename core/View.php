fe23e4bd168172599d8aeb539819d96ee57be08a
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-06-06 15:02:07 +0000

    Add the ability to pass data to the view

--- core/View.php
+++ core/View.php
@@ -8,14 +8,16 @@ class View
      * Render a view file.
      *
      * @param string $view Relative path to the view file.
+     * @param array $data Data passing into the view.
      * @return void
      */
-    public static function render(string $view): void
+    public static function render(string $view, array $data = []): void
     {
         $basePath = defined('BASE_PATH') ? BASE_PATH : realpath(__DIR__);
         $viewPath = "$basePath/app/views/$view";
 
         if (is_readable($viewPath)) {
+            extract($data, EXTR_SKIP);
             require $viewPath;
         }
     }
