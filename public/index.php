ff571e02b176bc0f725f1fd2fc9d606eed76ac1a
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-06-07 09:20:14 +0000

    Add autoload section to composer.json

--- public/index.php
+++ public/index.php
@@ -4,25 +4,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 define('BASE_PATH', realpath(__DIR__ . '/../'));
 
-spl_autoload_register(function ($class) {
-    $roots = [
-        BASE_PATH . '/app/'  => 'App\\',
-        BASE_PATH . '/core/' => 'Core\\',
-    ];
-    foreach ($roots as $baseDir => $prefix) {
-        $len = strlen($prefix);
-        if (strncmp($prefix, $class, $len) !== 0) {
-            continue;
-        }
-        $relativeClass = substr($class, $len);
-
-        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
-        if (file_exists($file)) {
-            require $file;
-        }
-    }
-});
-
 $router = new \Core\Router();
 $router->add('', [
     'controller' => 'home',
