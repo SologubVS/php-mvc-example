dccdf351fdc9d14610efcdde9f60b936ab91b2ef
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-06-25 04:07:30 +0000

    Add initial Databases controller class

---
+++ app/Controllers/Databases.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\AbstractController;
+
+class Databases extends AbstractController
+{
+    /**
+     * Show the index page.
+     *
+     * @return void
+     */
+    public function indexAction(): void
+    {
+    }
+}
