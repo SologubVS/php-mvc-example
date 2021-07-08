e319ff7ae2be365c9160b46d6cff3364989193eb
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-07-08 06:25:56 +0000

    Extend Logger with PSR-3 AbstractLogger

--- core/Logger.php
+++ core/Logger.php
@@ -2,7 +2,9 @@
 
 namespace Core;
 
-class Logger
+use Psr\Log\AbstractLogger;
+
+class Logger extends AbstractLogger
 {
     /**
      * Absolute path to the log file.
@@ -19,5 +21,19 @@ class Logger
     public function __construct(string $path)
     {
         $this->path = $path;
+    }
+
+    /**
+     * Add a log record at an arbitrary level.
+     *
+     * @param string $level Log level.
+     * @param string $message Log message.
+     * @param array $context Context data.
+     * @return void
+     *
+     * @throws \Psr\Log\InvalidArgumentException
+     */
+    public function log($level, $message, array $context = []): void
+    {
     }
 }
