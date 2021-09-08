fc2e519e2bf3ea4048a2ae8208b1a1c64e911a86
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-09-08 06:23:56 +0000

    Use some namespaces in front controller

--- public/index.php
+++ public/index.php
@@ -1,18 +1,21 @@
 <?php
 
+use Core\Exception\Debug\Environment as Debug;
+use Core\Exception\Handler as ExceptionHandler;
 use Core\Routing\RouteParameters as Route;
+use Core\Routing\Router;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 Dotenv\Dotenv::createImmutable(dirname(__DIR__))->safeLoad();
 
-$isDebug = Core\Exception\Debug\Environment::get();
-$handler = new Core\Exception\Handler(new Core\Logger(__DIR__ . '/../logs/app.log'), $isDebug);
+$logger  = new Core\Logger(__DIR__ . '/../logs/app.log');
+$handler = new ExceptionHandler($logger, Debug::get());
 $handler->register();
 
 Core\View::addPath(__DIR__ . '/../app/views');
 
-$router = new Core\Routing\Router('/', 'App\Controllers');
+$router = new Router('/', 'App\Controllers');
 $router->add('/', [
     Route::CONTROLLER => 'Home',
     Route::ACTION     => 'index',
