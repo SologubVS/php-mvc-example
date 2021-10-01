e98a7d2e25a94ed9f9cc1f501018ae6af0b23a60
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-10-01 09:16:08 +0000

    Add model records interface

---
+++ core/ModelRecordsInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core;
+
+interface ModelRecordsInterface extends CollectionInterface
+{
+    /**
+     * Filter records by the given column value pair.
+     *
+     * @param string $column Column for filtering.
+     * @param mixed $value Column value.
+     * @return static An instance of the filtered collection of records.
+     */
+    public function where(string $column, $value);
+}
