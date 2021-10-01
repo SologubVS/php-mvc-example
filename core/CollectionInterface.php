c9a435ca1177204e37c81fadfe6ea0784b03c18f
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-10-01 08:51:25 +0000

    Add collection interface

---
+++ core/CollectionInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Core;
+
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+
+interface CollectionInterface extends ArrayAccess, IteratorAggregate, Countable
+{
+    /**
+     * Filter the collection using the specified callback.
+     *
+     * @param callable $callback The callback function to use.
+     * @return static A new instance of the filtered collection.
+     */
+    public function filter(callable $callback);
+
+    /**
+     * Get the value of the first element in the collection.
+     *
+     * @return mixed
+     */
+    public function first();
+}
