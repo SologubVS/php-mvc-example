68fdb27116357c6230793d25e60693e9ea339799
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-10-01 09:11:10 +0000

    Add Collection class

---
+++ core/Collection.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core;
+
+use ArrayObject;
+
+class Collection extends ArrayObject implements CollectionInterface
+{
+    /**
+     * Create a new collection.
+     *
+     * @param array $array An array to create the collection.
+     */
+    public function __construct(array $array = [])
+    {
+        parent::__construct($array);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filter(callable $callback)
+    {
+        return new static(array_filter((array) $this, $callback));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function first()
+    {
+        $storage = (array) $this;
+        return array_shift($storage);
+    }
+}
