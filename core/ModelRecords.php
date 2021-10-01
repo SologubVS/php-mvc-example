b63105879952acc553be57217ce04f76cda3a30e
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-10-01 09:37:12 +0000

    Add ModelRecords class

---
+++ core/ModelRecords.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Core;
+
+class ModelRecords extends Collection implements ModelRecordsInterface
+{
+    /**
+     * Create a new model records collection.
+     *
+     * @param array[] $array An array of arrays to create the collection.
+     */
+    public function __construct(array $array = [])
+    {
+        parent::__construct($array);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function where(string $column, $value)
+    {
+        return $this->filter(function ($record) use ($column, $value) {
+            return $record[$column] == $value;
+        });
+    }
+}
