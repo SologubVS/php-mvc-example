57bef5c103d3e158585d345f6c907fc398735677
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-10-04 04:15:03 +0000

    Remove constructor method from ModelRecords class

--- core/Entities/ModelRecords.php
+++ core/Entities/ModelRecords.php
@@ -7,16 +7,6 @@ use Core\Support\Collection;
 class ModelRecords extends Collection implements ModelRecordsInterface
 {
     /**
-     * Create a new model records collection.
-     *
-     * @param array[] $array An array of arrays to create the collection.
-     */
-    public function __construct(array $array = [])
-    {
-        parent::__construct($array);
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function where(string $column, $value)
