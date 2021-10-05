87f7c11a95f27c5e9569ac6eac573c981a9ac4ee
Vitaliy Sologub <sologub-vs+git@yandex.com>
2021-10-05 07:32:41 +0000

    Add the ability to get posts by their slugs

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,17 +16,33 @@ class Post extends AbstractModel
     {
         return new ModelRecords([
             [
+                'slug' => 'post-1',
                 'title' => 'Post #1',
                 'content' => 'Pellentesque venenatis tempor elementum. Suspendisse pretium in.',
             ],
             [
+                'slug' => 'post-2',
                 'title' => 'Post #2',
                 'content' => 'Praesent elementum tellus non lorem feugiat dictum. Ut pharetra.',
             ],
             [
+                'slug' => 'post-3',
                 'title' => 'Post #3',
                 'content' => 'Proin varius tellus at euismod porttitor. Quisque sit amet duis.',
             ],
         ]);
+    }
+
+    /**
+     * Get a post record by its slug or throw an exception.
+     *
+     * @see \Core\Entities\AbstractModel::getOrFail()
+     *
+     * @param string $slug Post slug.
+     * @return array Post record.
+     */
+    public static function getBySlug(string $slug): array
+    {
+        return static::getOrFail('slug', $slug);
     }
 }
